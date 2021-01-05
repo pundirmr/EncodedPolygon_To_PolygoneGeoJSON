378cf05d83c5b9805811915f3b7acd0d7434c171
pundirmr <m.pundir@cityzenith.com>
2021-01-05 08:52:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// compare 2 arrays
+function areIdentical(a,b) {
+  return a.length === b.length && a.every(function(v,i) {
+    return v === b[i];
+  });
+}
+
+// accepts a Feature with a or just a LineString geometry.
+function lineToPolygon(f) {
+  var geometry, firstVertex, lastVertex;
+
+  if (f.type === 'Feature'){
+    geometry = f.geometry;
+  } else {
+    geometry = f;
+  }
+
+  if (geometry.type !== 'LineString') {
+    throw new Error('Only Linestring geometry type is supported.');
+  }
+
+  if (geometry.coordinates.length === 0) {
+    throw new Error('Empty geometry.');
+  }
+
+  firstVertex = geometry.coordinates[0];
+  lastVertex = geometry.coordinates[geometry.coordinates.length - 1];
+
+  if (!areIdentical(firstVertex, lastVertex)) geometry.coordinates.push(firstVertex);
+
+  if (geometry.coordinates.length < 4) {
+    throw new Error('A Polygon needs to have 4 or more positions.')
+  }
+
+  geometry.type = 'Polygon';
+  geometry.coordinates = [geometry.coordinates];
+
+  return f;
+};
